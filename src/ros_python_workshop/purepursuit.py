0a96512bfd03d1c1f628f308618cd8cfccd5f396
Evan Krell <ekrell@islander.tamucc.edu>
2018-12-06 23:21:36 -0600

    Working go2goal, not path

--- src/ros_python_workshop/purepursuit.py
+++ src/ros_python_workshop/purepursuit.py
@@ -1,317 +1,179 @@
 #!/usr/bin/env python
+
 import rospy
-from nav_msgs.msg import Odometry
+from turtlesim.msg import *
+from turtlesim.srv import *
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from std_srvs.srv import *
 import time, sys, math
 
+pose = { "x"     : None,
+         "y"     : None,
+         "theta" : None,
+       } 
 
-odometry_pose = None
-
-def makeWaypoint (x, y):
-    return { 'x': x,
-             'y': y
-    }
-
-def makePath (X, Y):
-    path = []
-
-    # If sizes mismatch, return empty list
-    if len(X) != len(Y):
-        return path
-
-    
-    for i in range(len(X)):
-        path.append(makeWaypoint(X[i], Y[i]))
-    return path
-
-def makePoint2D(x, y, theta):
-    return { 'x': x,
-             'y': y,
-             'theta': theta
-    }
-
-def odom2world(startPosWorld, currentPosOdom ):
-    # The turtlebot's odometer is initialized at [0, 0, 0, 0, 0, 0].
-    # We need it to be relative to the actual map.
-    # So we have to know what that [0, 0, 0, 0, 0, 0] actually is
-    # startPosWorld: robot's starting world position 
-    # currentPosOdom: robot's current odometry position
-
-    if (currentPosOdom == None):
-        return None
-
-    currentPosWorld = {
-    
-        "position": { "x": startPosWorld["position"]["x"] + 
-                          currentPosOdom.position.x,
-                      "y": startPosWorld["position"]["y"] + 
-                          currentPosOdom.position.y,
-                      "z": startPosWorld["position"]["z"] + 
-                          currentPosOdom.position.z, 
-        },
-
-        "orientation": { "x": startPosWorld["orientation"]["x"] + 
-                             currentPosOdom.orientation.x,
-                         "y": startPosWorld["orientation"]["y"] + 
-                             currentPosOdom.orientation.y,
-                         "z": startPosWorld["orientation"]["z"] + 
-                             currentPosOdom.orientation.z,
-                         "w": startPosWorld["orientation"]["w"] + 
-                             currentPosOdom.orientation.w,
-        },
-    }
-    
-
-    return currentPosWorld
-
-def printPoseDict(pose):
-    print ("Position")
-    print ("\t x:"), 
-    print (pose["position"]["x"])
-    print ("\t y:"), 
-    print (pose["position"]["y"])
-    print ("\t z:"), 
-    print (pose["position"]["z"])
-
-    print ("Orientation")
-    print ("\t x:"), 
-    print (pose["orientation"]["x"])
-    print ("\t y:"), 
-    print (pose["orientation"]["y"])
-    print ("\t z:"), 
-    print (pose["orientation"]["z"])
-    print ("\t w:"), 
-    print (pose["orientation"]["w"])
-
-def printPose2D (pose):
-    print ("(x: %d, y: %d, %.3f" \
-        % (pose["x"], pose["y"], pose["theta"]))
-
-# Start ROS Functions
 
 class turtlebot_velocity (object): 
-
-    _pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size = 10)
+    _pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
 
     def __init__(self):
         rospy.init_node('turtlebot_trajectoryFollow')
-        self.pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size = 10)
+        self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
         rospy.sleep(1)
 
     def write(self, linear, angular):
-        self._pub.publish(Twist(Vector3(linear, 0, 0),
-            Vector3(0, 0, angular)))
+        self._pub.publish(Twist(Vector3(linear, 0, 0), Vector3(0, 0, angular)))
 
-def turtlebot_odometry_callback (msg):
-    global odometry_pose
-    odometry_pose = msg.pose.pose
+def turtlebot_odometry_callback(msg):
+    global pose
+    pose["x"]     = msg.x
+    pose["y"]     = msg.y
+    pose["theta"] = msg.theta
 
 def listener():
-    return rospy.Subscriber("odom", Odometry, turtlebot_odometry_callback)
+    return rospy.Subscriber("/turtle1/pose", Pose, turtlebot_odometry_callback)
 
-# End ROS Functions
+def setParams(params):
+    if rospy.has_param("min_dist_goal"):
+        params["min_dist_goal"] = rospy.get_param("min_dist_goal")
+    else:
+        rospy.set_param("min_dist_goal", params["min_dist_goal"])
+    if rospy.has_param("dist_wheel_center"):
+        params["dist_wheel_center"] = rospy.get_param("dist_wheel_center")
+    else:
+        rospy.set_param("dist_wheel_center", params["dist_wheel_center"])
+    if rospy.has_param("k"):
+        params["k"] = rospy.get_param("k")
+    else:
+        rospy.set_param("k", params["k"])
+    if rospy.has_param("look_ahead"):
+        params["look_ahead"] = rospy.get_param("look_ahead")
+    else:
+        rospy.set_param("look_ahead", params["look_ahead"])
+    if rospy.has_param("kp"):
+        params["kp"] = rospy.get_param("kp")
+    else:
+        rospy.set_param("kp", params["kp"])
+    if rospy.has_param("l"):
+        params["l"] = rospy.get_param("l")
+    else:
+        rospy.set_param("l", params["l"])
+    if rospy.has_param("linear_speed"):
+        params["linear_speed"] = rospy.get_param("linear_speed")
+    else:
+        rospy.set_param("linear_speed", params["linear_speed"])
+    if rospy.has_param("goal"):
+        params["goal"] = rospy.get_param("goal")
+    else:
+        rospy.set_param("goal", params["goal"])
 
-# Start PurePursuit function
+def updateParams(params):
+    if rospy.has_param("min_dist_goal"):
+        params["min_dist_goal"] = rospy.get_param("min_dist_goal")
+    if rospy.has_param("dist_wheel_center"):
+        params["dist_wheel_center"] = rospy.get_param("dist_wheel_center")
+    if rospy.has_param("k"):
+        params["k"] = rospy.get_param("k")
+    if rospy.has_param("look_ahead"):
+        params["look_ahead"] = rospy.get_param("look_ahead")
+    if rospy.has_param("kp"):
+        params["kp"] = rospy.get_param("kp")
+    if rospy.has_param("l"):
+        params["l"] = rospy.get_param("l")
+    if rospy.has_param("linear_speed"):
+        params["linear_speed"] = rospy.get_param("linear_speed")
+    if rospy.has_param("goal"):
+        params["goal"] = rospy.get_param("goal")
+   
+def main():
 
-def dot2 (v, u):
-    return v['x'] * u['x'] + v['y'] * u['y']
+    ##############
+    # Parameters #
+    ##############
+    params = { "min_dist_goal"     : 0.5,
+               "dist_wheel_center" : 0.005,
+               "k"                 : 0.1,
+               "look_ahead"        : 1,
+               "kp"                : 1.0,
+               "l"                 : 2.9,
+               "linear_speed"      : 5,
+               "goal"              : (9, 9),
+             }
+    setParams(params)
 
-def dist (a, b):
-    return pow ( \
-        (b['x'] - a['x']) * (b['x'] - a['x']) + \
-        (b['y'] - a['y']) * (b['y'] - a['y']), 
-    0.5)
+    #########
+    # Setup #
+    #########
 
-def distP2S (currentPosition, waypointA, waypointB):
-    # Obtains difference between current position and segment s
-    # Returns the shortest distance from position to segment, and the point
-    # of the segment that gives the shortest distance
+    # Initialize ROS connections
+    velctrl = turtlebot_velocity()
+    sub = listener();
+    # Start timer (for logging, etc)
+    startTime = time.time()
+    iteration = 0
+    # Give ROS a chance to start up
+    rospy.sleep (1)
+    # Init inf distance to goal 
+    dist2Goal = float("inf")
 
-    c1 = 0.0
-    c2 = 0.0
-    di = 0.0
-    b = 0.0
+    # Start PurePursuit loop
+    while (True):
+        # Slight delay
+        rospy.sleep(0.05)
+        # Get updated parameters from parameter server
+        if iteration % 100 == 0:
+            updateParams(params)
 
-    waypointV = makeWaypoint(waypointB['x'] - waypointA['x'], 
-                             waypointB['y'] - waypointA['y'])
-    waypointW = makeWaypoint(currentPosition['x'] - waypointA['x'], 
-                             currentPosition['y'] - waypointA['y'])
-    waypointRes = makeWaypoint(-1, -1)
+        #########
+        # SENSE #
+        #########
 
-    c1 = dot2 (waypointW, waypointV)
+        # Get robot current position
+        if (pose == None):
+            continue
 
-    if (c1 <= 0.0):
-        di = dist(currentPosition, waypointA)
-        waypointRes = makeWaypoint(waypointA['x'], waypointA['y'])
-        return waypointRes, di
-        
-    if (c2 <= c1):
-        di = dist(currentPosition, waypointB)
-        waypointRes = makeWaypoint(waypointB['x'], waypointB['y'])
-        return waypointRes, di
-        
-    b = c1 / c2
-    waypointRes = makeWaypoint(wayPointA['x'] + b * waypointV['x'],
-                               wayPointA['y'] + b * waypointV['y'])
-    di = dist (currentPosition, waypointRes)
-    return waypointRes, di
-    
+        ###########
+        # Process #
+        ###########
 
-def pp_getPoint(pose, path, currentIdx, lookAhead):
-    # Returns a point on the path that is at the lookAhead distance
+        # Distance to goal
+        dist2goal = pow(((pose["x"]-params["goal"][0])*(pose["x"]-params["goal"][0]) + \
+                         (pose["y"]-params["goal"][1])*(pose["y"]-params["goal"][1])), 0.5)
+        if (dist2goal < params["min_dist_goal"]):
+            continue
 
-    closestIdx = 0
+        # Calculate curvature
+        dx = pose['x'] - params["goal"][0]
+        dy = pose['y'] - params["goal"][1]
+        x1 = math.cos(pose["theta"]) * dx + math.sin(pose["theta"]) * dy 
+        y1 = -1 * math.sin(pose["theta"]) * dx + math.cos(pose["theta"]) * dy
 
-    target = { "x": -1, "y": -1 }
-    potentialTarget = { "x": -1, "y": -1 }
-    targetPosition = { "x": None, "y": None, "theta": None }
+        if (x1 * x1 + y1 * y1) == 0:
+            curv = 0
+        else:
+            curv = (2.0 / (x1 * x1 + y1 * y1)) * (-1 * y1)
 
-    dmin = float("inf")
-    d = 0.0
-    d1 = 0.0
-    d2 = 0.0
+        ###########
+        # CONTROL #
+        ###########
 
-    # Number of waypoints
-    numWaypoints = len(path)
+        # Send movement control to robot
+        velctrl.write(params["linear_speed"], curv * params["linear_speed"])
+        # Update iteration
+        iteration = iteration + 1
 
-    # Init segment as length of path
-    segments = [0.0 for i in range(numWaypoints)]
+   
 
-    '''
-    Part 1: Find closest segment, and closest point on segment
-    '''
-
-    # From the current waypoint to final waypoint,
-    for i in range(currentIdx, numWaypoints - 1):
-
-        # Length of segment
-        segments[i] = dist (path[i], path[i + 1])
-
-        # Find closest point on segment
-        potentialTarget, d = distP2S(pose, path[i],  path[i + 1])
-
-        # Check if closest segment
-        if d < dmin:
-            target["x"] = potentialTarget["x"]
-            target["y"] = potentialTarget["y"]
-            dmin = d
-            closestIdx = i
-
-    # Check if waypoint was changed
-    if currentIdx != closestIdx:
-        currentIdx = closestIdx
-        #if closestIdx == numWaypoints - 2 # If is penultimate waypoint
-            
-
-    '''
-    Part 2: Find segment ahead in lookahead distance
-    '''
-    
-    d1 = dist (target, path[closestIdx + 1]) # MIGHT be 'currentIdx'
-    
-    # Index of lookahead point segment
-    k = closestIdx
-    while d1 < lookAhead and k + 1 < numWaypoints - 1:
-        # Searched point on this segment
-        k = k + 1
-        d1 = d1 + segments[k]
-    
-
-    '''
-    Part 3: Obtain t parameter in the segment
-    '''
-
-    d2 = d1 - lookAhead
-    t = (segments[k] - d2) / segments[k]
-
-    # Obtain point with t parameter
-    targetPosition['x'] = path[k]['x'] + \
-                          (path[k + 1]['x'] - path[k]['x']) * t
-    targetPosition['y'] = path[k]['y'] + \
-                          (path[k + 1]['y'] - path[k]['y']) * t
-
-    angleSegment = math.atan2(path[k + 1]['y'] - path[k]['y'],
-                         path[k + 1]['x'] - path[k]['x'])
-
-    targetPosition['theta'] = angleSegment
-    
-
-    return targetPosition, k
-
-
-# End PurePursuit functions
-
-
-def main ():
+def main_2 ():
 
     ###########
     # Options #
     ###########
 
-    startPosWorld = {
-        "position": { "x": 150,
-                      "y": 200,
-                      "z": 2.01, 
-        },
-        "orientation": { "x": 0,
-                         "y": 0,
-                         "z": 0,
-                         "w": 0,
-        },
-    }
- 
-    roll, pitch, yaw = euler_from_quaternion ( [\
-            startPosWorld['orientation']['x'],
-            startPosWorld['orientation']['y'],
-            startPosWorld['orientation']['z'],
-            startPosWorld['orientation']['w'] ])
-    worldPose2D = makePoint2D (startPosWorld["position"]["x"], 
-                               startPosWorld["position"]["y"], 
-                               yaw)
-
-    Xwaypoints = [startPosWorld['position']['x'], 170.0, 200.0, 210.0]
-    Ywaypoints = [startPosWorld['position']['y'], 210.0, 160.0, 160.0]
-    path = makePath(Xwaypoints, Ywaypoints)
-
-    minDist2Goal = 5;
-
-    # Pure Pursuit
-    dist_wheel_to_center = 0.005
-    k = 0.1
-    lookAhead = 1
-    Kp = 1.0
-    L = 2.9 
-    
-    linearSpeed = 0.1
-
-    #########
-    # SETUP #
-    #########
-
-    # Start timer (for logging, etc)
-    startTime = time.time()
-
-    # Give ROS a chance to start up
-    rospy.sleep (1)
-
-    # halt boolean to control when to stop tracking
-    halt = False
-
-    # Initiallize velocity controller
-    velctrl = turtlebot_velocity()
-
-    # Start listening to odometer
-    sub = listener();
-
-    # Set goal location as final point in path
-    goal = path[len(path) - 1]
-
     # Set initial distance to goal as max,
     # since it is yet unmeasured.
     # (Don't want it set to 0 since that would signal arrival)
-    dist2Goal = float("inf")
 
     waypoint = None
 
@@ -327,58 +189,13 @@ def main ():
     # Start PurePursuit loop
     while (halt == False):
         rospy.sleep(0.05)
-
-        #########
-        # SENSE #
-        #########
-
-        # Get robot current position
-        if (odometry_pose == None):
-            continue
-        worldPose = odom2world (startPosWorld, odometry_pose)
-        roll, pitch, yaw = euler_from_quaternion ( [\
-            worldPose['orientation']['x'],
-            worldPose['orientation']['y'],
-            worldPose['orientation']['z'],
-            worldPose['orientation']['w'] ])
-        worldPose2D = makePoint2D (worldPose["position"]["x"], 
-                                   worldPose["position"]["y"], 
-                                   yaw)
-        ###########
+        #print (odometry_pose)
+        velctrl.write(100, 100)
+###########
         # PROCESS #
         ###########
 
         
-        # Use pure pursuit to get angle to move toward goal
-        targetPoint, pathIdx = pp_getPoint(worldPose['position'], path, pathIdx, lookAhead)
-
-        dist2target = dist (worldPose['position'], targetPoint)
-        dist2goal = dist (worldPose['position'], goal)
-        if (dist2goal < minDist2Goal):
-            halt = True
-            continue
-
-        theta = yaw
-        # Calculate curvature
-        dx = worldPose['position']['x'] - targetPoint['x']
-        dy = worldPose['position']['y'] - targetPoint['y']
-        x1 = math.cos(theta) * dx + math.sin(theta) * dy 
-        y1 = -1 * math.sin(theta) * dx + math.cos(theta) * dy
-
-        if (x1 * x1 + y1 * y1) == 0:
-            curv = 0
-        else:
-            curv = (2.0 / (x1 * x1 + y1 * y1)) * (-1 * y1)
-
-        iterationCount = iterationCount + 1
-
-        ###########
-        # CONTROL #
-        ###########
-
-        # Send movement control to robot
-        velctrl.write(linearSpeed, curv * linearSpeed)
-
         #############
         # VISUALIZE #
         #############
---
+++ src/ros_python_workshop/purepursuit_gazebo.py
@@ -0,0 +1,421 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import time, sys, math
+
+
+odometry_pose = None
+
+def makeWaypoint (x, y):
+    return { 'x': x,
+             'y': y
+    }
+
+def makePath (X, Y):
+    path = []
+
+    # If sizes mismatch, return empty list
+    if len(X) != len(Y):
+        return path
+
+    
+    for i in range(len(X)):
+        path.append(makeWaypoint(X[i], Y[i]))
+    return path
+
+def makePoint2D(x, y, theta):
+    return { 'x': x,
+             'y': y,
+             'theta': theta
+    }
+
+def odom2world(startPosWorld, currentPosOdom ):
+    # The turtlebot's odometer is initialized at [0, 0, 0, 0, 0, 0].
+    # We need it to be relative to the actual map.
+    # So we have to know what that [0, 0, 0, 0, 0, 0] actually is
+    # startPosWorld: robot's starting world position 
+    # currentPosOdom: robot's current odometry position
+
+    if (currentPosOdom == None):
+        return None
+
+    currentPosWorld = {
+    
+        "position": { "x": startPosWorld["position"]["x"] + 
+                          currentPosOdom.position.x,
+                      "y": startPosWorld["position"]["y"] + 
+                          currentPosOdom.position.y,
+                      "z": startPosWorld["position"]["z"] + 
+                          currentPosOdom.position.z, 
+        },
+
+        "orientation": { "x": startPosWorld["orientation"]["x"] + 
+                             currentPosOdom.orientation.x,
+                         "y": startPosWorld["orientation"]["y"] + 
+                             currentPosOdom.orientation.y,
+                         "z": startPosWorld["orientation"]["z"] + 
+                             currentPosOdom.orientation.z,
+                         "w": startPosWorld["orientation"]["w"] + 
+                             currentPosOdom.orientation.w,
+        },
+    }
+    
+
+    return currentPosWorld
+
+def printPoseDict(pose):
+    print ("Position")
+    print ("\t x:"), 
+    print (pose["position"]["x"])
+    print ("\t y:"), 
+    print (pose["position"]["y"])
+    print ("\t z:"), 
+    print (pose["position"]["z"])
+
+    print ("Orientation")
+    print ("\t x:"), 
+    print (pose["orientation"]["x"])
+    print ("\t y:"), 
+    print (pose["orientation"]["y"])
+    print ("\t z:"), 
+    print (pose["orientation"]["z"])
+    print ("\t w:"), 
+    print (pose["orientation"]["w"])
+
+def printPose2D (pose):
+    print ("(x: %d, y: %d, %.3f" \
+        % (pose["x"], pose["y"], pose["theta"]))
+
+# Start ROS Functions
+
+class turtlebot_velocity (object): 
+
+    _pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size = 10)
+
+    def __init__(self):
+        rospy.init_node('turtlebot_trajectoryFollow')
+        self.pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size = 10)
+        rospy.sleep(1)
+
+    def write(self, linear, angular):
+        self._pub.publish(Twist(Vector3(linear, 0, 0),
+            Vector3(0, 0, angular)))
+
+def turtlebot_odometry_callback (msg):
+    global odometry_pose
+    odometry_pose = msg.pose.pose
+
+def listener():
+    return rospy.Subscriber("odom", Odometry, turtlebot_odometry_callback)
+
+# End ROS Functions
+
+# Start PurePursuit function
+
+def dot2 (v, u):
+    return v['x'] * u['x'] + v['y'] * u['y']
+
+def dist (a, b):
+    return pow ( \
+        (b['x'] - a['x']) * (b['x'] - a['x']) + \
+        (b['y'] - a['y']) * (b['y'] - a['y']), 
+    0.5)
+
+def distP2S (currentPosition, waypointA, waypointB):
+    # Obtains difference between current position and segment s
+    # Returns the shortest distance from position to segment, and the point
+    # of the segment that gives the shortest distance
+
+    c1 = 0.0
+    c2 = 0.0
+    di = 0.0
+    b = 0.0
+
+    waypointV = makeWaypoint(waypointB['x'] - waypointA['x'], 
+                             waypointB['y'] - waypointA['y'])
+    waypointW = makeWaypoint(currentPosition['x'] - waypointA['x'], 
+                             currentPosition['y'] - waypointA['y'])
+    waypointRes = makeWaypoint(-1, -1)
+
+    c1 = dot2 (waypointW, waypointV)
+
+    if (c1 <= 0.0):
+        di = dist(currentPosition, waypointA)
+        waypointRes = makeWaypoint(waypointA['x'], waypointA['y'])
+        return waypointRes, di
+        
+    if (c2 <= c1):
+        di = dist(currentPosition, waypointB)
+        waypointRes = makeWaypoint(waypointB['x'], waypointB['y'])
+        return waypointRes, di
+        
+    b = c1 / c2
+    waypointRes = makeWaypoint(wayPointA['x'] + b * waypointV['x'],
+                               wayPointA['y'] + b * waypointV['y'])
+    di = dist (currentPosition, waypointRes)
+    return waypointRes, di
+    
+
+def pp_getPoint(pose, path, currentIdx, lookAhead):
+    # Returns a point on the path that is at the lookAhead distance
+
+    closestIdx = 0
+
+    target = { "x": -1, "y": -1 }
+    potentialTarget = { "x": -1, "y": -1 }
+    targetPosition = { "x": None, "y": None, "theta": None }
+
+    dmin = float("inf")
+    d = 0.0
+    d1 = 0.0
+    d2 = 0.0
+
+    # Number of waypoints
+    numWaypoints = len(path)
+
+    # Init segment as length of path
+    segments = [0.0 for i in range(numWaypoints)]
+
+    '''
+    Part 1: Find closest segment, and closest point on segment
+    '''
+
+    # From the current waypoint to final waypoint,
+    for i in range(currentIdx, numWaypoints - 1):
+
+        # Length of segment
+        segments[i] = dist (path[i], path[i + 1])
+
+        # Find closest point on segment
+        potentialTarget, d = distP2S(pose, path[i],  path[i + 1])
+
+        # Check if closest segment
+        if d < dmin:
+            target["x"] = potentialTarget["x"]
+            target["y"] = potentialTarget["y"]
+            dmin = d
+            closestIdx = i
+
+    # Check if waypoint was changed
+    if currentIdx != closestIdx:
+        currentIdx = closestIdx
+        #if closestIdx == numWaypoints - 2 # If is penultimate waypoint
+            
+
+    '''
+    Part 2: Find segment ahead in lookahead distance
+    '''
+    
+    d1 = dist (target, path[closestIdx + 1]) # MIGHT be 'currentIdx'
+    
+    # Index of lookahead point segment
+    k = closestIdx
+    while d1 < lookAhead and k + 1 < numWaypoints - 1:
+        # Searched point on this segment
+        k = k + 1
+        d1 = d1 + segments[k]
+    
+
+    '''
+    Part 3: Obtain t parameter in the segment
+    '''
+
+    d2 = d1 - lookAhead
+    t = (segments[k] - d2) / segments[k]
+
+    # Obtain point with t parameter
+    targetPosition['x'] = path[k]['x'] + \
+                          (path[k + 1]['x'] - path[k]['x']) * t
+    targetPosition['y'] = path[k]['y'] + \
+                          (path[k + 1]['y'] - path[k]['y']) * t
+
+    angleSegment = math.atan2(path[k + 1]['y'] - path[k]['y'],
+                         path[k + 1]['x'] - path[k]['x'])
+
+    targetPosition['theta'] = angleSegment
+    
+
+    return targetPosition, k
+
+
+# End PurePursuit functions
+
+
+def main ():
+
+    ###########
+    # Options #
+    ###########
+
+    startPosWorld = {
+        "position": { "x": 150,
+                      "y": 200,
+                      "z": 2.01, 
+        },
+        "orientation": { "x": 0,
+                         "y": 0,
+                         "z": 0,
+                         "w": 0,
+        },
+    }
+ 
+    print (startPosWorld)
+    roll, pitch, yaw = euler_from_quaternion ( [\
+            startPosWorld['orientation']['x'],
+            startPosWorld['orientation']['y'],
+            startPosWorld['orientation']['z'],
+            startPosWorld['orientation']['w'] ])
+    worldPose2D = makePoint2D (startPosWorld["position"]["x"], 
+                               startPosWorld["position"]["y"], 
+                               yaw)
+
+    Xwaypoints = [startPosWorld['position']['x'], 170.0, 200.0, 210.0]
+    Ywaypoints = [startPosWorld['position']['y'], 210.0, 160.0, 160.0]
+    path = makePath(Xwaypoints, Ywaypoints)
+
+    minDist2Goal = 5;
+
+    # Pure Pursuit
+    dist_wheel_to_center = 0.005
+    k = 0.1
+    lookAhead = 1
+    Kp = 1.0
+    L = 2.9 
+    
+    linearSpeed = 0.1
+
+    #########
+    # SETUP #
+    #########
+
+    # Start timer (for logging, etc)
+    startTime = time.time()
+
+    # Give ROS a chance to start up
+    rospy.sleep (1)
+
+    # halt boolean to control when to stop tracking
+    halt = False
+
+    # Initiallize velocity controller
+    velctrl = turtlebot_velocity()
+
+    # Start listening to odometer
+    sub = listener();
+
+    # Set goal location as final point in path
+    goal = path[len(path) - 1]
+
+    # Set initial distance to goal as max,
+    # since it is yet unmeasured.
+    # (Don't want it set to 0 since that would signal arrival)
+    dist2Goal = float("inf")
+
+    waypoint = None
+
+    pathIdx = 0
+
+    # Counter for # times the main follower
+    # loop has iterated. Used to control how often the
+    # visualization/log messages display.
+    iterationCount = 0
+
+
+
+    # Start PurePursuit loop
+    while (halt == False):
+        rospy.sleep(0.05)
+
+        #########
+        # SENSE #
+        #########
+
+        # Get robot current position
+        if (odometry_pose == None):
+            continue
+        worldPose = odom2world (startPosWorld, odometry_pose)
+        roll, pitch, yaw = euler_from_quaternion ( [\
+            worldPose['orientation']['x'],
+            worldPose['orientation']['y'],
+            worldPose['orientation']['z'],
+            worldPose['orientation']['w'] ])
+        worldPose2D = makePoint2D (worldPose["position"]["x"], 
+                                   worldPose["position"]["y"], 
+                                   yaw)
+        ###########
+        # PROCESS #
+        ###########
+
+        
+        # Use pure pursuit to get angle to move toward goal
+        targetPoint, pathIdx = pp_getPoint(worldPose['position'], path, pathIdx, lookAhead)
+
+        dist2target = dist (worldPose['position'], targetPoint)
+        dist2goal = dist (worldPose['position'], goal)
+        if (dist2goal < minDist2Goal):
+            halt = True
+            continue
+
+        theta = yaw
+        # Calculate curvature
+        dx = worldPose['position']['x'] - targetPoint['x']
+        dy = worldPose['position']['y'] - targetPoint['y']
+        x1 = math.cos(theta) * dx + math.sin(theta) * dy 
+        y1 = -1 * math.sin(theta) * dx + math.cos(theta) * dy
+
+        if (x1 * x1 + y1 * y1) == 0:
+            curv = 0
+        else:
+            curv = (2.0 / (x1 * x1 + y1 * y1)) * (-1 * y1)
+
+        iterationCount = iterationCount + 1
+
+        ###########
+        # CONTROL #
+        ###########
+
+        # Send movement control to robot
+        velctrl.write(linearSpeed, curv * linearSpeed)
+
+        #############
+        # VISUALIZE #
+        #############
+
+        visTime = time.time()
+        runTime = visTime - startTime
+        if (iterationCount % 100 == 0):
+            print ("")
+            #printPoseDict (worldPose)
+            print ("Robot position: (x: %d, y: %d, theta: %.3f)" \
+                % (worldPose["position"]["x"], worldPose["position"]["y"], theta))
+            print ("Next waypoint: (x: %d, y: %d, segment: %d)" \
+                % (targetPoint['x'], targetPoint['y'], pathIdx))
+            print ("Distance to: (waypoint: %.3f, goal: %.3f)" \
+                % (dist2target, dist2goal))
+            print ("Time traveled: %.3f s" \
+                % (runTime))
+            print ("")
+
+    ##########
+    # FINISH #
+    ##########
+    
+    # Halt robot
+    velctrl.write(0, 0)
+
+    endTime = time.time()
+    runTime = endTime - startTime
+    print ("Goal reached in: ", runTime, "seconds")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
